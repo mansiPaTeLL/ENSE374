af97f524390168d7bc772a1cc35bf382f6c5f9ef
Mansi Patel <mansi811@icloud.com>
2018-10-14 00:23:38 -0600

    Adding ListElement.java file

---
+++ 374-Lab1/src/ListElement.java
@@ -0,0 +1,17 @@
+
+public class ListElement {
+	private ListElement next;
+	private int data;
+	
+	public ListElement () {
+		this.data = 0;
+		this.next = null;
+		
+	}
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+}
