d62cb2eb236cf95d27c68aebd6a268f15286a881
mansiPateLL <mansi811@icloud.com>
2018-10-26 12:46:08 -0600

    added more methods - get & delete method

--- 374-Lab1/src/LinkedList.java
+++ 374-Lab1/src/LinkedList.java
@@ -1,35 +1,77 @@
 public class LinkedList {
 	private ListElement head, tail;
-	private ListElement temp_le;
+	private ListElement listelement;
     public int counter;
 
 	    public LinkedList() 
 	    {
 	    	head = null;
 	    	tail = null;
-	    	temp_le = null;
+	    	listelement = null;
 	    	counter = 0 ;
 		}
 	    
 	    
 	    public void addElement(ListElement le)
 	    {
-			temp_le = new ListElement();
-			temp_le.setData(le.getData());
+	    	listelement = new ListElement();
+	    	listelement.setData(le.getData());
 			
 			if (head == null)
 			{
-				this.head = this.tail = this.temp_le;
+				this.head = this.tail = this.listelement;
 			}
 			
 			else {
-				this.tail.setNext(this.temp_le);
-				this.tail = this.temp_le;
+				this.tail.setNext(this.listelement);
+				this.tail = this.listelement;
 			}
 			
 			counter++;
-			
 	    }
-	
+			
+		public ListElement getElement(int index) {	
+				if(index > counter) 
+					return null;
+				
+				//Start from the head and iterate to the requested index.
+				listelement = head;
+				
+				for(int i = 0; i < index; i++) {
+					listelement = listelement.getNext();
+				}
+				
+				return listelement;
+			}
+		
+		public ListElement deleteElement(int index)
+		{
+			if (index > counter)
+				return null;
+			
+			ListElement temp;
+			temp = listelement;
+			temp = null;
+			
+			if (index == 0 ) 
+			{
+				head = null;
+			}
+			else 
+		    {
+				for (int i = 0; i <index; i++) {
+					temp = listelement;
+					listelement = listelement.getNext();
+				}
+		
+			}
+			if (temp != null) {
+				temp.setNext(listelement.getNext()); }
+			    counter --;
+			    temp = listelement;
+			    listelement = null;
+			    return temp;
+
+		}	
 }
 
